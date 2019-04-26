a2265af8f8d5ba02fc1aeacfb4de8fb26946fe5f
shelbylcass <32856713+shelbylcass@users.noreply.github.com>
2019-04-26 13:04:13 -0400

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-;
 
 public class EvenOdd{
 
@@ -10,6 +9,7 @@ public int totalGames;
 public int totalWins;	
 public String directions="Welcome to Even or Odd";
 private GetInput input= new GetInput();
+private Random rand= new Random();
 
 	public EvenOdd(){
 
@@ -24,18 +24,19 @@ private GetInput input= new GetInput();
 	public int playGame(){
 
 		//get user choice
-		System.out.println("Let's play Even or Odd")
+		System.out.println("Let's play Even or Odd");
 		totalGames++;
 		guess = input.getBinaryInput("Enter your guess. ","0 for even","1 for odd" );
 		//get user throw
 		userThrow= input.getInt("Enter number to throw", 0,5);
 		compThrow=rand.nextInt(6);
 		//show result
-		System.out.println("the computer threw a ",compThrow);
+		System.out.print("the computer threw a ");
+		System.out.println(compThrow);
 		if ((userThrow+compThrow)%2 == 0 ){
-			System.out.prinlnt("the sum is even.");
+			System.out.print("the sum is even.");
 		}
-		else{System.out.println("the sum is odd.")}
+		else{System.out.print("the sum is odd.");}
 		//show winner
 		if ((userThrow+compThrow)%2 == guess ){
 			System.out.println("YOU WON");
