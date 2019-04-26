64bdf8c34d7b4832d44133d7a7b2d429766cb5c6
shelbylcass <32856713+shelbylcass@users.noreply.github.com>
2019-04-26 13:01:18 -0400

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+;
+
+public class EvenOdd{
+
+private int guess;
+private int userThrow;
+private int compThrow;
+public int totalGames;
+public int totalWins;	
+public String directions="Welcome to Even or Odd";
+private GetInput input= new GetInput();
+
+	public EvenOdd(){
+
+	}
+
+	public void launchGame(){
+		System.out.println(directions);
+		while(playGame()==1){ }
+
+	}
+
+	public int playGame(){
+
+		//get user choice
+		System.out.println("Let's play Even or Odd")
+		totalGames++;
+		guess = input.getBinaryInput("Enter your guess. ","0 for even","1 for odd" );
+		//get user throw
+		userThrow= input.getInt("Enter number to throw", 0,5);
+		compThrow=rand.nextInt(6);
+		//show result
+		System.out.println("the computer threw a ",compThrow);
+		if ((userThrow+compThrow)%2 == 0 ){
+			System.out.prinlnt("the sum is even.");
+		}
+		else{System.out.println("the sum is odd.")}
+		//show winner
+		if ((userThrow+compThrow)%2 == guess ){
+			System.out.println("YOU WON");
+			totalWins++;
+		}
+		else{
+			System.out.println("YOU LOST");
+		}
+		//ask to play again
+		return input.getBinaryInput("Would you like to play again?","Enter 1 to play Even or Odd again", "Enter 0 to return to main menu");
+
+	}
+
+}
