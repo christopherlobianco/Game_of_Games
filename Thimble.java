f3d2fdab4f7a8d4a3ce56b4563bd351f58c1a062
aladamski <49822843+aladamski@users.noreply.github.com>
2019-04-29 00:10:39 -0400

    Add files via upload
    Working

--- Thimble.java
+++ Thimble.java
@@ -1,22 +1,20 @@
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Thimble {
 
     private int guess;
     public int totalWins;
     public int totalGames;
-    private int randcomp=1;
+    private int randcomp=2;
     private int bestoutof;
     private GetInput input = new GetInput();
-    private Random rand = new Random();
 
     public Thimble(){
-
     }
 
     public void directions(){
         System.out.println("Welcome to Find the Thimble! To play, you will" +
-                " choose the 'Best out of Number'. You will hae to guess the" +
+                " choose the 'Best out of Number'. You will have to guess the" +
                 " correct hand more than half of that number.");
     }
 
@@ -28,12 +26,14 @@ public class Thimble {
     public int playGame(){
         System.out.println("Guess which hand the Thimble is in!");
         bestoutof = input.getOddInt("Please choose the 'Best out of Number (this number should be an odd" +
-                " number and not negative): ",1,1000);
+                " number and not negative)",1,1000);
         totalGames++;
         while(bestoutof>0){
             bestoutof--;
-            guess = input.getBinaryInput("To choose a hand, input: ","'1' for Left", "'2' for Right",1,2);
-            if (guess == rand.nextInt(randcomp)){
+            int computer = ThreadLocalRandom.current().nextInt(1,3);
+            //System.out.println("Correct Answer: "+ computer);
+            guess = input.getBinaryInput("To choose a hand, input: ","'1' for Left", "'2' for Right",'1','2');
+            if ( (guess-48) == computer){
                 System.out.println("You chose wisely!");
                 totalWins++;
             }
@@ -47,4 +47,9 @@ public class Thimble {
 
         return input.getInt("Would you like to play again? \nEnter 1 to play Coin Toss again \nEnter 0 to return to main menu",0,1);
     }
+
+//    public static void main(String args[]) {
+//        Thimble thim = new Thimble();
+//        thim.launchGame();
+//    }
 }
