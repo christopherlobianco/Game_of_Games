065b92cfc3182f55c5130a852f67e6e89c456a5b
shelbylcass <32856713+shelbylcass@users.noreply.github.com>
2019-04-29 17:29:41 -0400

    Update CoinToss.java

--- CoinToss.java
+++ CoinToss.java
@@ -27,7 +27,7 @@ public int playGame(){
 	System.out.println("Let's play Coin Toss!");
 	totalGames++;
 	//get number of rounds
-	int rounds=input.getOddInt(
+	int totalRounds=input.getOddInt(
 		"Enter the number of rounds you'd like to play",
 		1, //lowbound
 		maxRounds); //highbound
@@ -37,30 +37,53 @@ public int playGame(){
 		"enter t for tails", 
 		'h', 
 		't',
-		rounds);
+		totalRounds);
 	//play rounds
-	for(int i=0; i<rounds;i++){
+	int round=0;
+	int roundsWon=0;
+	while(round<totalRounds && roundsWon<(totalRounds/2+1) ){
 		//generate coin toss
-		flip= rand.nextInt(2)+1;
-		//display result
+		flip= rand.nextInt(2);
+		//display flip result
 		System.out.print("The coin landed on ");
 		System.out.println(faces[flip]);
 		//compare guess
-		if(guesses[i]==flip){
-			System.out.println("You Won!");
-			totalWins++;
+		if(guesses[round]==flip){
+			System.out.println("You Won this round!");
+			roundsWon++;
 		}
 		else{
-			System.out.println("You Lost");
+			System.out.println("You Lost this round");
 		}
+		round++;
 		
 	}
+
+	//display game results
 	System.out.println("End of Game.");
+	if(roundsWon>(totalRounds/2)){
+		System.out.println("You won the game.");
+		totalWins++;
+	}
+	else{
+		System.out.println("You lost the game.");
+	}
 	//ask to play again
 	return input.getInt("Would you like to play again? \nEnter 1 to play Coin Toss again \nEnter 0 to return to main menu",0,1);
 
 }
 
+
+
+
+public static void main(String argv[]){
+	CoinToss game= new CoinToss();
+	game.launchGame();
+	System.out.print("total wins ");
+	System.out.println(game.totalWins);
+	System.out.print("total games ");
+	System.out.println(game.totalGames);
+}
 	
 
 }
