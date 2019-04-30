3f8f35ae85db621eb1eb5653ea37469fdd1676a7
christopherlobianco <christopher.lobianco15@gmail.com>
2019-04-30 12:24:30 -0400

    Update Thimble.java

--- Thimble.java
+++ Thimble.java
@@ -27,25 +27,33 @@ public class Thimble {
         System.out.println("Guess which hand the Thimble is in!");
         bestoutof = input.getOddInt("Please choose the 'Best out of Number (this number should be an odd" +
                 " number and not negative)",1,1000);
+        int totalGuesses = bestoutof;
         totalGames++;
+        int numberWon = 0;
         while(bestoutof>0){
             bestoutof--;
             int computer = ThreadLocalRandom.current().nextInt(1,3);
             //System.out.println("Correct Answer: "+ computer);
             guess = input.getBinaryInput("To choose a hand, input: ","'1' for Left", "'2' for Right",'1','2');
-            if ( (guess-48) == computer){
+            if (guess == computer){
                 System.out.println("You chose wisely!");
-                totalWins++;
+                numberWon++;
             }
             else {
                 System.out.println("Wrong!");
             }
         }
+        if(numberWon > (totalGuesses-numberWon)) {
+            totalWins++;
+            System.out.println("You have won this game of Find the Thimble");
+        } else {
+            System.out.println("You have lost this game of Find the Thimble");
+        }
         System.out.println("End of Game.");
 
         //ask to play again
 
-        return input.getInt("Would you like to play again? \nEnter 1 to play Coin Toss again \nEnter 0 to return to main menu",0,1);
+        return input.getInt("Would you like to play again? \nEnter 1 to play Find the Thimble again \nEnter 0 to return to main menu",0,1);
     }
 
 //    public static void main(String args[]) {
